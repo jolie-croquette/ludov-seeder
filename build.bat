d71c9d0884c5d23847e7bd56eb69985cd09ce1a4
Jolie_Croquette <83880530+jolie-croquette@users.noreply.github.com>
2025-09-16 15:49:01 -0400

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo === Compilation du projet en exe ===
-python -m PyInstaller --onefile --name LUDOVSeeder main.py
-echo === Terminé ! Le .exe est disponible dans le dossier dist/ ===
-pause
